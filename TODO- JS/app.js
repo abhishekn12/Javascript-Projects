4153052fb315880449dc8b336f1824fa031317d9
abhishekn12 <nabhishek865@gmail.com>
2021-07-04 19:52:06 +0530

    htmlcss

---
+++ TODO- JS/app.js
@@ -0,0 +1,5 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click',addTodo);
